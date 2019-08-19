9d195764bfd1f2da7dc50ed73e70f3e5ac8610f5
Philip K <philip@warpmail.net>
2019-08-19 14:32:58 +0200

    updated header tags

--- 0x0.el
+++ 0x0.el
@@ -1,8 +1,8 @@
 ;;; 0x0.el --- Upload to 0x0.st -*- lexical-binding: t -*-
 
 ;; Author: Philip K. <philip@warpmail.net>
-;; Version: 0.1.0
-;; Keywords: faces
+;; Version: 0.1.1
+;; Keywords: comm
 ;; Package-Requires: ((emacs "24.1"))
 ;; URL: https://git.sr.ht/~zge/nullpointer-emacs
 
