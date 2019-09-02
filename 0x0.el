87efcc058f40e8a38a613cb479a2affba9f0ebe5
Philip K <philip@warpmail.net>
2019-09-02 11:29:14 +0200

    added support for uploading via curl

--- 0x0.el
+++ 0x0.el
@@ -58,21 +58,54 @@ See https://github.com/lachs0r/0x0/blob/master/cleanup.py#L13"
 See https://github.com/lachs0r/0x0/blob/master/fhost.py#L22"
   :type 'float)
 
+(defcustom 0x0-use-curl-if-installed t
+  "Maximal permitted file size.
+
+See https://github.com/lachs0r/0x0/blob/master/fhost.py#L22"
+  :type 'boolean)
+
 (defvar 0x0--filename nil)
 
-(defun 0x0--request-data (start end boundary)
-  "Prepare a HTTP Body to upload the current buffer from START to END.
+(defun 0x0--calculate-timeout (size)
+  "Calculate days a file of size SIZE would last.
 
-The string BOUNDARY will be used to as the multipart boundary."
-  (let ((source (current-buffer))
-        (filename (or 0x0--filename (buffer-name))))
-    (with-temp-buffer
-      (insert "--" boundary "\r\n"
-              "Content-Disposition: form-data; name=\"file\"; filename=\"" filename "\"\r\n"
-              "Content-type: text/plain\r\n\r\n")
-      (insert-buffer-substring-no-properties source start end)
-      (insert "\r\n--" boundary "--")
-      (buffer-string))))
+See also `0x0-min-age' and `0x0-max-age'."
+  (+ 0x0-min-age
+     (* (- 0x0-min-age 0x0-max-age)
+        (expt (- (/ size 0x0-max-size) 1.0) 3))))
+
+(defun 0x0--use-curl (start end)
+  "Backend function for uploading using curl.
+
+Operate on region between START and END."
+  (let ((buf (generate-new-buffer "*0x0 response*")))
+    (call-process-region start end "curl"
+                         nil buf nil
+                         "-s" "-S" "-F"
+                         (concat "file=@-;filename="
+                                 0x0--filename)
+                         0x0-url)
+    buf))
+
+(defun 0x0--use-url (start end)
+  "Backend function for uploading using `url' functions.
+
+Operate on region between START and END."
+  (let* ((boundary (format "%X-%X-%X" (random) (random) (random)))
+         (url-request-extra-headers
+          `(("Content-Type" . ,(concat "multipart/form-data; boundary=" boundary))))
+         (url-request-data
+          (let ((source (current-buffer))
+                (filename (or 0x0--filename (buffer-name))))
+            (with-temp-buffer
+              (insert "--" boundary "\r\n"
+                      "Content-Disposition: form-data; name=\"file\"; filename=\"" filename "\"\r\n"
+                      "Content-type: text/plain\r\n\r\n")
+              (insert-buffer-substring-no-properties source start end)
+              (insert "\r\n--" boundary "--")
+              (buffer-string))))
+         (url-request-method "POST"))
+    (url-retrieve-synchronously 0x0-url)))
 
 ;;;###autoload
 (defun 0x0-upload (start end)
@@ -81,23 +114,19 @@ The string BOUNDARY will be used to as the multipart boundary."
 If START and END are not specified, upload entire buffer."
   (interactive (list (if (use-region-p) (region-beginning) (point-min))
                      (if (use-region-p) (region-end) (point-max))))
-  (let* ((boundary (format "%X-%X-%X" (random) (random) (random)))
-         (url-request-extra-headers
-          `(("Content-Type" . ,(concat "multipart/form-data; boundary=" boundary))))
-         (url-request-data (0x0--request-data start end boundary))
-         (url-request-method "POST")
-         (timeout (+ 0x0-min-age
-                     (* (- 0x0-min-age 0x0-max-age)
-                        (expt (- (/ (- end start) 0x0-max-size) 1.0) 3)))))
-    (url-retrieve 0x0-url
-                  (lambda (_status)
-                    (if (search-forward-regexp (concat "^" (regexp-quote 0x0-url)) nil t)
-                        (let ((url (buffer-substring (line-beginning-position) (line-end-position))))
-                          (kill-new url)
-                          (message "Yanked `%s' into kill ring. Should last ~%2g days"
-                                   url timeout))
-                      (error "Failed to upload"))))
-    nil))
+  (let ((resp (if (and 0x0-use-curl-if-installed
+                       (executable-find "curl"))
+                  (0x0--use-curl start end)
+                (0x0--use-url start end)))
+        (timeout (0x0--calculate-timeout (- end start))))
+    (with-current-buffer resp
+      (goto-char (point-min))
+      (unless (search-forward-regexp (concat "^" (regexp-quote 0x0-url)) nil t)
+        (error "Failed to upload"))
+      (kill-new (buffer-string))
+      (message "Yanked `%s' into kill ring. Should last ~%2g days"
+               (buffer-string) timeout))
+    (kill-buffer resp)))
 
 ;;;###autoload
 (defun 0x0-upload-file (file)
