f4365512d45ca1a7b6deb93e2fbc19fbc2df1b20
Philip K <philipk@posteo.net>
2020-11-01 22:39:51 +0100

    Extract tests from main file

--- 0x0.el
+++ 0x0.el
@@ -28,7 +28,6 @@
 ;; https://liberapay.com/lachs0r/donate if you like the service.
 
 (require 'url)
-(require 'ert)
 
 ;;; Code:
 
@@ -278,33 +277,6 @@ SERVICE must be a member of `0x0-services'."
     (setq-local 0x0--current-host service)
     (pop-to-buffer (current-buffer)))
   (message "Press C-c C-c to upload."))
-
-(defun 0x0--test-service (service)
-  "Upload and retrieve a random string for SERVICE."
-  (let ((rand (make-string 256 0)))
-    (dotimes (i (length rand))
-      (setf (aref rand i) (+ ?a (random (- ?z ?a)))))
-    (let* ((resp (0x0-upload-string rand service))
-           result)
-      (with-current-buffer (url-retrieve-synchronously resp)
-        (goto-char (point-min))
-        (forward-paragraph)
-        (delete-region (point-min) (1+ (point)))
-        (setq result (buffer-string)))
-      (should (or (equal rand result)
-                  (save-match-data
-                    (and (string-match "[[:space:]]*\\'" result)
-                         (replace-match "" nil nil result))))))))
-
-(dolist (service (mapcar #'car 0x0-services))
-  (let ((name (intern (format "0x0-test-%s" (symbol-name service))))
-        (name* (intern (format "0x0-test-%s-curl" (symbol-name service)))))
-    (eval `(ert-deftest ,name ()
-             (let ((0x0-use-curl-if-installed nil))
-               (0x0--test-service ',service))))
-    (eval `(ert-deftest ,name* ()
-             (let ((0x0-use-curl-if-installed t))
-               (0x0--test-service ',service))))))
 
 (provide '0x0)
 
