517c984c82b770c3ccf5c28608da442d91f6e252
William Vaughn <vaughnwilld@gmail.com>
2021-06-30 20:32:03 -0700

    add tests for response handler
    make 0x0-test-- the prefix for all tests.

--- 0x0-test.el
+++ 0x0-test.el
@@ -6,7 +6,7 @@
 (require 'ert)
 (require 'cl-macs)
 
-(defun 0x0--test-service (server)
+(defun 0x0-test--call-service (server)
   "Upload and retrieve a random string for SERVER."
   (let ((rand (make-string 256 0)))
     (dotimes (i (length rand))
@@ -28,22 +28,22 @@
 ;;; Integration tests that actually call the services.
 (dolist (server-key (mapcar #'car 0x0-servers))
   (let* ((server (cdr (assq server-key 0x0-servers)))
-         (name (intern (format "0x0-test-%s-curl" server-key))))
+         (name (intern (format "0x0-test--%s-curl" server-key))))
     (ert-set-test name
                   (make-ert-test
                    :name name
                    :tags '(:integration)
-                   :body (lambda () (0x0--test-service server))))))
+                   :body (lambda () (0x0-test--call-service server))))))
 
 ;; Developer unit tests
-(ert-deftest 0x0-unit-calculate-timeout ()
+(ert-deftest 0x0-test--calculate-timeout ()
   "should calculate an estimated file timeout based on file size."
   (let ((server (list :min-age 30 :max-age 365 :max-size (* 1024 1024 512))))
     (should (equal (0x0--calculate-timeout server (* 1024 1024 512)) 30.0))
     (should (equal (round (0x0--calculate-timeout server 1)) 365))
     (should (equal (0x0--calculate-timeout server 268435456) 71.875))))
 
-(ert-deftest 0x0-unit-send-fallback-to-url ()
+(ert-deftest 0x0-test--unit-send-fallback-to-url ()
   (cl-letf (((symbol-function 'executable-find) (lambda (cmd)
                                                   (should (equal cmd "curl"))
                                                   (ert-fail "Unexpected call to executable-find")))
@@ -61,7 +61,7 @@
                (0x0--send server "upload.txt" bounds)
                "success!")))))
 
-(ert-deftest 0x0-unit-use-curl-if-installed ()
+(ert-deftest 0x0-test--use-curl-if-installed ()
   (cl-letf (((symbol-function 'executable-find) (lambda (cmd)
                                                   (should (equal cmd "curl"))))
             ((symbol-function '0x0--url) (lambda (_ca _b)
@@ -81,6 +81,38 @@
       (should (equal
                (0x0--send server "upload.txt" bounds)
                "success!")))))
+
+(ert-deftest 0x0-test--handle-response-success ()
+  (let ((server (list
+                 :scheme "https"
+                 :host "example.com"
+                 :curl-args-fun #'ignore))
+        (resp-txt "Some garbage nobody cares about. But, the good stuff is below.
+
+https://example.com/abcdefghi.jpg
+
+Other stuff."))
+    (with-temp-buffer
+      (rename-buffer "*resp test*")
+      (insert resp-txt)
+      (should
+       (equal (0x0--handle-resp server 1 (current-buffer))
+              "https://example.com/abcdefghi.jpg"))
+      (should (equal (current-kill 0) "https://example.com/abcdefghi.jpg"))
+      (should-not (buffer-live-p "*resp test*")))))
+
+(ert-deftest 0x0-tst--handle-response-failure ()
+  (let ((server (list
+                 :scheme "https"
+                 :host "example.com"
+                 :curl-args-fun #'ignore))
+        (resp-txt "Some garbage nobody cares about."))
+    (with-temp-buffer
+      (rename-buffer "*resp test*")
+      (insert resp-txt)
+      (should-error (0x0--handle-resp server 1 (current-buffer))
+                    :type 'error)
+      (should (equal (buffer-name) "*resp test*")))))
 
 (provide '0x0-test)
 ;;; 0x0-test.el ends here
