ecde71038eff81ed97df905b4151b8aee1feeca1
William Vaughn <vaughnwilld@gmail.com>
2021-06-30 21:24:20 -0700

    add attribution for Philip (pkal)
    update commentary for melpa

--- 0x0.el
+++ 0x0.el
@@ -26,7 +26,14 @@
 ;;; Commentary:
 ;;
 ;; Integration with https://0x0.st, envs.sh, ttm.sh, and self-hosted services
-;; from Emacs. Intended for sharing images, files, and blocks of code in ERC.
+;;
+;; Upload whatever you need to a pastebin server. The commands
+;; `0x0-upload-file',`0x0-upload-text' and `0x0-upload-kill-ring', which
+;; respectively upload (parts of) the current buffer, a file on your disk and a
+;; string from the minibuffer can be used too.
+;;
+;; The default host and the host this package is named after is https://0x0.st.
+;; Consider donating to https://liberapay.com/mia/donate if you like the service.
 
 (require 'url)
 
